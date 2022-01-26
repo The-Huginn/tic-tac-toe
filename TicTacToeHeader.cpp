9e405794a3130137ca4884809445fc8d6a96b9a5
Rastislav Budinsky <rastislavbudinsky@gmail.com>
2022-01-26 17:21:02 +0100

    Add project files.

---
+++ TicTacToeHeader.cpp
@@ -0,0 +1,5 @@
+/**
+* @file TicTacToeHeader.cpp
+* @brief This file implements the interface for playing the game Tic Tac Toe
+* @author Rastislav Budinsky
+*/
---
+++ TicTacToeHeader.h
@@ -0,0 +1,7 @@
+#pragma once
+/**
+* @file TicTacToeHeader.h
+* @brief This file contains interface for interacting and playing the game Tic Tac Toe.
+* Game is implemented using Singleton, new game automatically destroys any previous ones.
+* @author Rastislav Budinsky
+*/
